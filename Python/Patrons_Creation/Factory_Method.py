c7767b5b76574de8b60340d537c9966ad8eb70c2
yohan <yohanbinan22@gmail.com>
2024-04-04 07:51:42 +0200

    I've implemented Factory_Method in python

---
+++ Python/Patrons_Creation/Factory_Method.py
@@ -0,0 +1,67 @@
+import tkinter as tk 
+from abc import ABC, abstractmethod
+
+class Widget:
+    @abstractmethod
+    def get_widget():
+        pass
+
+class LabelWidget(Widget):
+    def __init__(self, master, **kwargs):
+        self.widget = tk.Label(master, **kwargs)
+
+    def get_widget(self):
+        return self.widget
+
+class ButtonWidget(Widget):
+    def __init__(self, master, **kwargs):
+        self.widget = tk.Button(master, **kwargs)
+
+    def get_widget(self):
+        return self.widget
+
+class EntryWidget(Widget):
+    def __init__(self, master, **kwargs):
+        self.widget = tk.Entry(master, **kwargs)
+
+    def get_widget(self):
+        return self.widget
+    
+
+
+class WidgetFactory:
+    @staticmethod
+    @abstractmethod
+    def create_widget(master, **kwargs):
+        pass
+    
+class LabelFactory(WidgetFactory):
+    def create_widget(master, **kwargs):
+        return LabelWidget(master, **kwargs)
+
+class ButtonFactory(WidgetFactory):
+    def create_widget(master, **kwargs):
+        return ButtonWidget(master, **kwargs)
+
+class EntryFactory(WidgetFactory):
+    def create_widget(master, **kwargs):
+        return EntryWidget(master, **kwargs)
+        
+    
+def main():
+    root = tk.Tk()
+    root.geometry("300x200")
+
+    label = LabelFactory.create_widget(root, text="Hello, World!").get_widget()
+    label.pack()
+
+    button = ButtonFactory.create_widget(root, text="Click Me!").get_widget()
+    button.pack()
+
+    entry = EntryFactory.create_widget(root).get_widget()
+    entry.pack()
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
