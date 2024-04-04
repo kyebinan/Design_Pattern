626d367061a6d629439c5e29e5b141d7c4487394
yohan <yohanbinan22@gmail.com>
2024-04-04 23:47:29 +0200

    Working on the WinFactory and the MacFactory

--- Python/Patrons_Creation/Abstract_Factory.py
+++ Python/Patrons_Creation/Abstract_Factory.py
@@ -39,3 +39,62 @@ class MacLabelWidget(LabelWidget):
 
     def get_widget(self):
         return self.widget
+    
+
+class GUIFactory(ABC):
+    @staticmethod
+    @abstractmethod
+    def create_button(master, **kwargs):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def create_label(master, **kwargs):
+        pass
+
+
+class WinFactory(GUIFactory):
+
+    def create_button(master, **kwargs):
+        pass
+
+    
+    def create_label(master, **kwargs):
+        pass
+
+
+class MacFactory(GUIFactory):
+
+    def create_button(master, **kwargs):
+        pass
+
+    
+    def create_label(master, **kwargs):
+        pass
+
+
+class Application:
+    def __init__(self, master, **kwargs):
+        pass
+
+
+
+
+def main():
+    # root = tk.Tk()
+    # root.geometry("300x200")
+
+    # label = LabelFactory.create_widget(root, text="Hello, World!").get_widget()
+    # label.pack()
+
+    # button = ButtonFactory.create_widget(root, text="Click Me!").get_widget()
+    # button.pack()
+
+    # entry = EntryFactory.create_widget(root).get_widget()
+    # entry.pack()
+
+    # root.mainloop()
+    pass
+
+if __name__ == "__main__":
+    main()
